5ee9b1b3ea2f4ecad8a659ff557531611907dbfe
thepenguin <nor@acorneroftheweb.com>
2022-11-28 13:42:05 +0100

    Now can generate boards, but it is potentially inf

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,25 +1,56 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Data.Aeson                           (FromJSON, ToJSON)
 import           Data.List                            (transpose)
+import           GHC.Generics
 import           Network.Wai.Middleware.RequestLogger
+import           Test.QuickCheck
 import           Web.Scotty
 
 type Board = [[Subboard]]
 
-type Subboard = [[Int]]
+type Subboard = [[Cell]]
 
-fetchRows :: Board -> [[Int]]
+data Cell = Empty | Val Int
+  deriving (Show, Generic)
+
+instance ToJSON Cell
+instance FromJSON Cell
+
+genSubBoard :: Gen Subboard
+genSubBoard = do
+  a <- shuffle [1 .. 9]
+  let b  = [[0,1,2],
+            [3,4,5],
+            [6,7,8]]
+  return $ map (map (\x -> Val (a !! x))) b
+
+
+genBoard :: Gen Board
+genBoard = do
+  a <- vectorOf 9 genSubBoard
+  let b  = [[0,1,2],
+            [3,4,5],
+            [6,7,8]]
+  let c = map (map (a !!)) b
+  if verify c then return c else genBoard
+
+fetchRows :: Board -> [[Cell]]
 fetchRows x = foldr (\y -> (++) $ map (concatMap (!! y)) x) [] [0,1,2]
 
-fetchCols :: Board -> [[Int]]
+fetchCols :: Board -> [[Cell]]
 fetchCols = fetchRows . transpose . map (map transpose)
 
 squareVerify :: Subboard -> Bool
 squareVerify = listVerify . concat
 
-listVerify :: [Int] -> Bool
-listVerify = (== 45) . sum
+listVerify :: [Cell] -> Bool
+listVerify = (== 45) . foldr (\x -> (+) $ case x of
+                                 Empty -> 0
+                                 Val i -> i
+                             ) 0
 
 verify :: Board -> Bool
 verify x = squares && rows && cols
@@ -33,41 +64,46 @@ main = scotty 3000 $ do
   middleware logStdout
 
   get "/generate" $ do
-    json $ (undefined :: String)
+    json (undefined :: Board)
+
+  get "/example" $ do
+    json example
 
   post "/solve" $ do
-    json $ True
+      board <- jsonData
+      json $ verify board
+
 
 example :: Board
 example = [
-             [[[3,1,7],
-               [9,2,5],
-               [4,6,8]],
-              [[2,5,4],
-               [1,8,6],
-               [3,9,7]],
-              [[8,9,6],
-               [4,7,3],
-               [1,5,2]]],
+             [[[Val 3, Val 1, Val 7],
+               [Val 9, Val 2, Val 5],
+               [Val 4, Val 6, Val 8]],
+              [[Val 2, Val 5, Val 4],
+               [Val 1, Val 8, Val 6],
+               [Val 3, Val 9, Val 7]],
+              [[Val 8, Val 9, Val 6],
+               [Val 4, Val 7, Val 3],
+               [Val 1, Val 5, Val 2]]],
 
 
-             [[[2,3,4],
-               [5,8,6],
-               [1,6,9]],
-              [[6,7,1],
-               [9,4,2],
-               [5,3,8]],
-              [[9,8,5],
-               [7,3,1],
-               [6,2,4]]],
+             [[[Val 2, Val 3, Val 4],
+               [Val 5, Val 8, Val 6],
+               [Val 1, Val 6, Val 9]],
+              [[Val 6, Val 7, Val 1],
+               [Val 9, Val 4, Val 2],
+               [Val 5, Val 3, Val 8]],
+              [[Val 9, Val 8, Val 5],
+               [Val 7, Val 3, Val 1],
+               [Val 6, Val 2, Val 4]]],
 
-             [[[8,9,1],
-               [7,4,2],
-               [6,5,3]],
-              [[4,2,3],
-               [8,6,5],
-               [7,1,9]],
-              [[5,6,7],
-               [3,1,9],
-               [2,4,8]]]
+             [[[Val 8, Val 9, Val 1],
+               [Val 7, Val 4, Val 2],
+               [Val 6, Val 5, Val 3]],
+              [[Val 4, Val 2, Val 3],
+               [Val 8, Val 6, Val 5],
+               [Val 7, Val 1, Val 9]],
+              [[Val 5, Val 6, Val 7],
+               [Val 3, Val 1, Val 9],
+               [Val 2, Val 4, Val 8]]]
           ]
