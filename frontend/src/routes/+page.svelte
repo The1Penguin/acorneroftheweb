ddb0c4c2723745e6268f020a871a7f7a854d125b
Pingu <nor@acorneroftheweb.com>
2022-12-02 17:00:06 +0100

    It centered ^^

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -15,34 +15,37 @@
   </title>
 </head>
 
-<article class="flex justify-center prose md:prose-lg lg:prose-xl">
-  <p>
-    Hello and welcome to this corner of the web.
-  </p>
-</article>
+<div class="flex flex-col items-center">
+  <article class="flex items-center prose md:prose-lg lg:prose-xl">
+    <p>
+      Hello and welcome to this corner of the web.
+    </p>
+  </article>
 
-<div class="grid-rows-3">
-{#await numbers then nums}
-  {#each nums as outer}
-    <div class="grid-cols-3 flex">
-    {#each outer as inner}
-      <div class="grid-rows-3 m-1">
-      {#each inner as outer2}
-        <div class="grid-cols-3">
-        {#each outer2 as inner2}
-          <div class="btn btn-square">
-            {#if inner2.tag == 'Val'}
-              {inner2.contents}
-            {:else}
-              0
-            {/if}
+  <div class="grid-rows-3">
+  {#await numbers then nums}
+    {#each nums as outer}
+      <div class="grid-cols-3 flex">
+      {#each outer as inner}
+        <div class="grid-rows-3 m-1">
+        {#each inner as outer2}
+          <div class="grid-cols-3">
+          {#each outer2 as inner2}
+            <div class="btn btn-square">
+              {#if inner2.tag == 'Val'}
+                {inner2.contents}
+              {:else}
+                0
+              {/if}
+            </div>
+          {/each}
           </div>
         {/each}
         </div>
       {/each}
       </div>
     {/each}
-    </div>
-  {/each}
-{/await}
+  {/await}
+  </div>
+
 </div>
