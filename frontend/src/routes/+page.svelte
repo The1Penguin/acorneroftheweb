88f03bf91b61a9ff20add290efe72a3d0292615c
Pingu <nor@acorneroftheweb.com>
2022-12-02 16:45:14 +0100

    Now displays as a grid

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -15,10 +15,34 @@
   </title>
 </head>
 
-<article class="flex justify-evenly prose md:prose-lg lg:prose-xl">
+<article class="flex justify-center prose md:prose-lg lg:prose-xl">
   <p>
     Hello and welcome to this corner of the web.
   </p>
 </article>
 
-<pre>{JSON.stringify(numbers, null, 2)}</pre>
+<div class="grid-rows-3">
+{#await numbers then nums}
+  {#each nums as outer}
+    <div class="grid-cols-3 flex">
+    {#each outer as inner}
+      <div class="grid-rows-3">
+      {#each inner as outer2}
+        <div class="grid-cols-3">
+        {#each outer2 as inner2}
+          <div class="btn">
+            {#if inner2.tag == 'Val'}
+              {inner2.contents}
+            {:else}
+              0
+            {/if}
+          </div>
+        {/each}
+        </div>
+      {/each}
+      </div>
+    {/each}
+    </div>
+  {/each}
+{/await}
+</div>
