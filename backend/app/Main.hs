94aeb6d4489b2ce38f1021d79a0dcc8b9d6f89ec
thepenguin <nor@acorneroftheweb.com>
2022-12-06 22:08:44 +0100

    Rewrote a lot and it is now way faster

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -3,22 +3,22 @@
 module Main where
 
 import           Control.Concurrent
-import           Control.Monad (replicateM)
+import           Control.Monad                        (replicateM)
 import           Control.Monad.IO.Class
 import           Data.Aeson                           (FromJSON, ToJSON)
 import           Data.IORef
-import           Data.List ( nub, transpose, elemIndex )
+import           Data.List                            (nub, transpose)
 import           Data.Maybe
 import           GHC.Generics
-import           Network.Wai.Middleware.RequestLogger
 import           Network.Wai.Middleware.Cors
+import           Network.Wai.Middleware.RequestLogger
+import           Prelude                              hiding (lookup)
+import           System.Random
+import           System.Random.Shuffle
 import           Test.QuickCheck
 import           Web.Scotty
-import           Prelude hiding (lookup)
 
-type Board = [[Subboard]]
-
-type Subboard = [[Cell]]
+type Board = [[Cell]]
 
 data Cell = Empty | Val Int
   deriving (Show, Generic, Eq)
@@ -26,77 +26,81 @@ data Cell = Empty | Val Int
 instance ToJSON Cell
 instance FromJSON Cell
 
-findEmpty :: Board -> Maybe (Int, Int, Int, Int)
-findEmpty board = if isJust index then Just (y1, x1, y2, x2) else Nothing
-  where
-    list  = concat $ concat $ concat board
-    index = elemIndex Empty list
-    indexFix = fromJust index
-    y1 = indexFix `div` 27
-    x1 = (`div` 9) $ indexFix `mod` 27
-    y2 = (`div` 3) $ indexFix `mod` 9
-    x2 = indexFix `mod` 3
+findEmpty :: Board -> [(Int, Int)]
+findEmpty board = concat $
+                [[
+                     (y,x)
+                   | x <- [0..8], board !! y !! x  == Empty]
+                | y <- [0..8]]
 
-genBoard :: Board -> [Int] -> Gen Board
-genBoard board [] = do
-  let a = [1..9]
-  b <- shuffle a
-  genBoard board b
-genBoard board vs =
-        if verify board then return board else do
-          let (y1, _, y2, _) = case findEmpty board of
-                Just (a,b,c,d) -> (a,b,c,d)
-                Nothing        -> (3,3,3,3)
-          let new =
-                [[[[
-                      if (board !! y !! x !! b !! a) /= Empty
-                      then board !! y !! x !! b !! a
-                      else
-                        (if y1==y && y2 == b
-                         then Val (vs !! (3*x+a))
-                         else Empty)
-                   | a <- [0..2]]
-                  | b <- [0..2]]
-                 | x <- [0..2]]
-                | y <- [0..2]]
-          if valid new then genBoard new [] else genBoard board []
+optionBoard :: Board -> (Int, Int) -> [Cell]
+optionBoard board (row, col) = [i | i <- map Val [1..9], i `notElem` left]
+  where
+    rows = fetchRows board
+    cols = fetchCols board
+    leftOnRow = filter (/= Empty) (rows !! row)
+    leftOnCol = filter (/= Empty) (cols !! col)
+    left = leftOnRow ++ leftOnCol
+
+genBoard :: IO Board
+genBoard = fromJust . solve empty <$> newStdGen
+
+updateBoard :: Board -> (Int, Int) -> Cell -> Board
+updateBoard board (y1,x1) val =
+                [[
+                      if y1 == y && x1 == x
+                      then val
+                      else board !! y !! x
+                 | x <- [0..8]]
+                | y <- [0..8]]
+
+solve :: Board -> StdGen -> Maybe Board
+solve board std
+  | not (valid board) = Nothing
+  | verify board = Just board
+  | otherwise = if null solutions then Nothing else head solutions
+    where
+      poss = findEmpty board
+      pos = head $ shuffle' poss (length poss) std
+      solutions = filter (/= Nothing)
+        [solve (updateBoard board pos val) std | val <- optionBoard board pos]
+
 
 removeBoard :: Board -> Int -> Gen Board
 removeBoard board 0 = return board
 removeBoard board amount = do
-          x1 <- elements [0..2]
-          x2 <- elements [0..2]
-          y1 <- elements [0..2]
-          y2 <- elements [0..2]
+          x1 <- elements [0..8]
+          y1 <- elements [0..8]
           let new =
-                [[[[
-                      if y1==y && x1 == x && y2 == b && x2 == a
+                [[
+                      if y1==y && x1 == x
                       then Empty
-                      else board !! y !! x !! b !! a
-                   | a <- [0..2]]
-                  | b <- [0..2]]
-                 | x <- [0..2]]
-                | y <- [0..2]]
+                      else board !! y !! x
+                 | x <- [0..8]]
+                | y <- [0..8]]
           removeBoard new (amount-1)
 
 
 fetchRows :: Board -> [[Cell]]
-fetchRows x = foldr (\y -> (++) $ map (concatMap (!! y)) x) [] [0..2]
+fetchRows = id
 
 fetchCols :: Board -> [[Cell]]
-fetchCols = fetchRows . transpose . map (map transpose)
+fetchCols = transpose
 
-squareVerify :: Subboard -> Bool
-squareVerify = listVerify . concat
+fetchSquares :: Board -> [[Cell]]
+fetchSquares = map concat . groupBy3 . concat . transpose . map groupBy3
+  where
+    groupBy3 :: [t] -> [[t]]
+    groupBy3 []         = []
+    groupBy3 [_]        = error "The sudoku board became malformed"
+    groupBy3 [_,_]      = error "The sudoku board became malformed"
+    groupBy3 (a:b:c:ds) = [a,b,c] : groupBy3 ds
 
 listVerify :: [Cell] -> Bool
 listVerify = (== 45) . foldr (\x -> (+) $ case x of
                                  Empty -> 0
                                  Val i -> i
                              ) 0
-
-squareValid :: Subboard -> Bool
-squareValid = listValid . concat
 
 listValid :: [Cell] -> Bool
 listValid x = a == b
@@ -107,16 +111,16 @@ listValid x = a == b
 valid :: Board -> Bool
 valid x = squares && rows && cols
   where
-    squares = and $ concatMap (map squareValid) x
-    rows = all listValid $ fetchRows x
-    cols = all listValid $ fetchCols x
+    squares = all listValid $ fetchSquares x
+    rows    = all listValid $ fetchRows x
+    cols    = all listValid $ fetchCols x
 
 verify :: Board -> Bool
-verify x = squares && rows && cols
+verify x = rows && cols
   where
-    squares = and $ concatMap (map squareVerify) x
-    rows = all listVerify $ fetchRows x
-    cols = all listVerify $ fetchCols x
+    -- squares = all listVerify $ fetchSquares x
+    rows    = all listVerify $ fetchRows x
+    cols    = all listVerify $ fetchCols x
 
 routes :: IORef [Board] -> ScottyM ()
 routes boards = do
@@ -138,56 +142,31 @@ routes boards = do
 
 update :: IORef [Board] -> [Board] -> IO ()
 update board list= do
-  a <- generate $ genBoard empty []
+  a <- genBoard
   writeIORef board (list ++ [a])
   putStrLn "Added a board to the list"
 
 main :: IO ()
 main = do
-  a <- replicateM 3 $ (generate . flip removeBoard 40)  =<< generate (genBoard empty [])
+  a <- replicateM 3 $ (generate . flip removeBoard 40) =<< genBoard
   print a
   c <- newIORef a
   scotty 3000 $ routes c
 
 example :: Board
-example = [
-             [[[Val 9, Val 6, Val 7],
-               [Val 4, Val 8, Val 1],
-               [Val 5, Val 2, Val 3]],
-              [[Val 1, Val 3, Val 2],
-               [Val 6, Val 5, Val 7],
-               [Val 4, Val 9, Val 8]],
-              [[Val 4, Val 5, Val 8],
-               [Val 2, Val 3, Val 9],
-               [Val 1, Val 6, Val 7]]],
-
-             [[[Val 2, Val 3, Val 4],
-               [Val 1, Val 5, Val 6],
-               [Val 7, Val 9, Val 8]],
-              [[Val 8, Val 1, Val 9],
-               [Val 7, Val 2, Val 3],
-               [Val 5, Val 4, Val 6]],
-              [[Val 5, Val 7, Val 6],
-               [Val 9, Val 8, Val 4],
-               [Val 3, Val 2, Val 1]]],
-
-             [[[Val 6, Val 4, Val 2],
-               [Val 3, Val 7, Val 9],
-               [Val 8, Val 1, Val 5]],
-              [[Val 9, Val 8, Val 5],
-               [Val 2, Val 6, Val 1],
-               [Val 3, Val 7, Val 4]],
-              [[Val 7, Val 1, Val 3],
-               [Val 8, Val 4, Val 5],
-               [Val 6, Val 9, Val 2]]]
-          ]
+example = [[Val 5, Val 3, Val 4, Val 6, Val 7, Val 8, Val 9, Val 1, Val 2],
+           [Val 6, Val 7, Val 2, Val 1, Val 9, Val 5, Val 3, Val 4, Val 8],
+           [Val 1, Val 9, Val 8, Val 3, Val 4, Val 2, Val 5, Val 6, Val 7],
+           [Val 8, Val 5, Val 9, Val 7, Val 6, Val 1, Val 4, Val 2, Val 3],
+           [Val 4, Val 2, Val 6, Val 8, Val 5, Val 3, Val 7, Val 9, Val 1],
+           [Val 7, Val 1, Val 3, Val 9, Val 2, Val 4, Val 8, Val 5, Val 6],
+           [Val 9, Val 6, Val 1, Val 5, Val 3, Val 7, Val 2, Val 8, Val 4],
+           [Val 2, Val 8, Val 7, Val 4, Val 1, Val 9, Val 6, Val 3, Val 5],
+           [Val 3, Val 4, Val 5, Val 2, Val 8, Val 6, Val 1, Val 7, Val 9]]
 
 
 empty :: Board
 empty = [
-         [
-          [
-           [Empty | _ <- [0..2 :: Integer]]
-          | _ <- [0..2 :: Integer]]
-         | _ <- [0..2 :: Integer]]
-        | _ <- [0..2 :: Integer]]
+          [ Empty
+         | _ <- [1..9 :: Integer]]
+        | _ <- [1..9 :: Integer]]
